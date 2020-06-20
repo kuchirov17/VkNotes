5174b7f4f661c19c8a809a44fb6bd55a38013923
sergeantIbra <54941210+sergeantChill@users.noreply.github.com>
2020-06-20 12:47:29 +0300

    First commit

---
+++ build/precache-manifest.8a5ade7f1b6354b1635f1a339ab224a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bdafbffa5ad7be9f90e9d23dde457c",
+    "url": "/VkNotes/index.html"
+  },
+  {
+    "revision": "da4123309039e9500c9b",
+    "url": "/VkNotes/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "82a256545efd06bb7247",
+    "url": "/VkNotes/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "da4123309039e9500c9b",
+    "url": "/VkNotes/static/js/2.23013b94.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/VkNotes/static/js/2.23013b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a256545efd06bb7247",
+    "url": "/VkNotes/static/js/main.d1066edd.chunk.js"
+  },
+  {
+    "revision": "9ea5fcb77d2b9e362a68",
+    "url": "/VkNotes/static/js/runtime-main.f14f218a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/VkNotes/static/media/persik.4e1ec840.png"
+  }
+]);
